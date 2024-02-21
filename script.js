54f7fd0cfb7a70cc5156b064fd124ed778cbd9e6
mmontero6 <149798340+mmontero6@users.noreply.github.com>
2024-02-21 15:35:51 +0000

    first

---
+++ script.js
@@ -0,0 +1,10 @@
+// let input = document.querySelector(".linkInput");
+let only =  document.querySelector(".only");
+let whole = document.querySelector("body");
+
+only.onclick =  function (){
+
+
+whole.style.background = "red";
+
+};
